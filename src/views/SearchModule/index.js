0077c0aa0efbad6b0826ecf5f2e2217e8ecffc50
许庆骁 <xu.qingxiao@iwhalecloud.com>
2020-10-17 00:14:43 +0800

    搜索链接

--- src/views/SearchModule/index.js
+++ src/views/SearchModule/index.js
@@ -1,27 +1,33 @@
-import React, { Component, useState, useEffect } from "react";
-import { Input, Tabs, Select } from "antd";
-import { connect } from "react-redux";
-import "./index.less";
-import { showDzdhChannelUrl, showAllChannelUrl } from "@/config/urls";
-import { queryChannelInfo } from "@/redux/Main/actions";
-import AxiosData from "@/utils/axios";
-import Keyword from "../../assets/images/keyword.png";
-import NoticeList from "../../component/NoticeList";
-import CopyImg from "@/component/CopyImg";
+import React, { useState, useEffect } from 'react';
+import { Input, Tabs, Select, message } from 'antd';
+import { connect } from 'react-redux';
+import './index.less';
+import {
+  showDzdhChannelUrl,
+  showAllChannelUrl,
+  showSearchUrl,
+} from '@/config/urls';
+import { queryChannelInfo } from '@/redux/Main/actions';
+import AxiosData from '@/utils/axios';
+import Keyword from '../../assets/images/keyword.png';
+import NoticeList from '../../component/NoticeList';
+import CopyImg from '@/component/CopyImg';
 
 const InputGroup = Input.Group;
 const { Option } = Select;
 const { Search } = Input;
 const { TabPane } = Tabs;
 const tabArr = [
-  { tabName: "知网", tabId: 1 },
-  { tabName: "泉方学术搜索", tabId: 2 },
-  { tabName: "本地PubMed", tabId: 3 },
-  { tabName: "节目检索", tabId: 4 },
+  { tabName: '知网', tabId: 1 },
+  { tabName: '泉方学术搜索', tabId: 2 },
+  { tabName: '本地PubMed', tabId: 3 },
+  { tabName: '节目检索', tabId: 4 },
 ];
 const SearchModule = (props) => {
   const [dzdhChannel, setDzdhChannel] = useState([]);
-
+  const [searchUrl, setSearchUrl] = useState({});
+  const [searchVal, setSearchVal] = useState('');
+  const [searchOption, setSearchOption] = useState('1');
   //计算组建宽度
   const queryWidthParam = (index) => {
     if (index < 3) {
@@ -64,43 +70,69 @@ const SearchModule = (props) => {
       });
   };
 
+  //获取搜索链接地址
+  const querySearchUrl = () => {
+    AxiosData.get(showSearchUrl)
+      .then((res) => {
+        setSearchUrl(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  //检索方法
+  const searchFn = (value) => {
+    console.log(value, '====');
+    if (value) {
+      if (searchOption === '1') {
+        window.open(
+          'http://search--chkd--cnki--net--http.cnki.resource.jsph.org.cn:2222/kns/Brief/singleResult.aspx?code=CHKD&kw=' +
+            value,
+        );
+      } else {
+        window.open(searchUrl[searchOption] + value);
+      }
+    } else {
+      message.warning('请输入检索词');
+      return;
+    }
+
+    setSearchVal(value);
+  };
+
+  const searchOptionFn = (value) => {
+    setSearchOption(value);
+    console.log(value);
+  };
+
   useEffect(() => {
     queryDzdhChannel();
     queryShowAllChannel();
+    querySearchUrl();
   }, []);
 
   return (
     <div className="main-area">
       <div className="search-module-area">
         <div className="search-module-container">
-          {/* <Tabs defaultActiveKey="1" onChange>
-            {tabArr.map((item, index) => {
-              return (
-                <TabPane tab={item.tabName} key={item.tabId}>
-                  <Search
-                    placeholder="请输入关键字..."
-                    enterButton="检索"
-                    size="large"
-                    onSearch={(value, tabId) => console.log(value, item.tabId)}
-                    style={{ width: 620 }}
-                  />
-                </TabPane>
-              );
-            })}
-          </Tabs> */}
-
           <InputGroup compact className="input-group-container">
-            <Select defaultValue="1" size="large" style={{ width: 120 }}>
+            <Select
+              defaultValue="1"
+              size="large"
+              style={{ width: 150 }}
+              onChange={searchOptionFn}
+            >
               <Option value="1">知网</Option>
-              <Option value="2">泉方学术搜索</Option>
-              <Option value="3">本地PubMed</Option>
-              <Option value="4">节目检索</Option>
+              <Option value="qfxs">泉方学术搜索</Option>
+              <Option value="pubmed">本地PubMed</Option>
+              {/* <Option value="4">节目检索</Option> */}
             </Select>
             <Search
               placeholder="请输入关键字..."
               enterButton="检索"
               size="large"
-              onSearch={(value, tabId) => console.log(value, item.tabId)}
+              onSearch={searchFn}
               style={{ width: 490 }}
             />
           </InputGroup>
